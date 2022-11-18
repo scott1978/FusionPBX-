0c44ccd2014ca0faaabe2d1987d222b2829ae0d1
fusionpbx-cn <33116308+scott1978@users.noreply.github.com>
2022-11-18 16:54:56 +0800

    Update environment.sh

--- centos7/resources/environment.sh
+++ centos7/resources/environment.sh
@@ -46,36 +46,36 @@ elif [ .$cpu_name = .'x86_64' ]; then
 	fi
 	cpu_architecture='x86'
 else
-	error "You are using an unsupported cpu '$cpu_name'"
+	error "您正在使用不受支持的cpu '$cpu_name'& You are using an unsupported cpu '$cpu_name'"
 	exit 3
 fi
 	
 if [ .$cpu_architecture = .'arm' ]; then
-	error "CentOS on arm is not supported at this time"
+	error " 不支持 arm CentOS 系统 & CentOS on arm is not supported at this time"
 	exit 3
 elif [ .$cpu_architecture = .'x86' ]; then
 	if [ .$os_mode = .'32' ]; then
-		error "You are using a 32bit OS this is unsupported"
+		error "不支持您使用的是32位操作系统 & You are using a 32bit OS this is unsupported"
 		if [ .$cpu_mode = .'64' ]; then
-			warning " Your CPU is 64bit you should consider reinstalling with a 64bit OS"
+			warning " 您的CPU是64位，您应该考虑使用64位操作系统重新安装 & Your CPU is 64bit you should consider reinstalling with a 64bit OS"
 		fi
 		exit 3
 	elif [ .$os_mode = .'64' ]; then
-		verbose "Correct CPU and Operating System detected"
+		verbose "检测到正确的CPU和操作系统 & Correct CPU and Operating System detected"
 	else
-		error "Unknown Operating System mode '$os_mode' is unsupported"
-		warning "Detected environment was :-"
-		warning "os_name:'$os_name'"
-		warning "os_mode:'$os_mode'"
-		warning "cpu_name:'$cpu_name'"
-		warning "cpu_architecture:'$cpu_architecture'"
+		error "不支持未知操作系统模式 '$os_mode' & Unknown Operating System mode '$os_mode' is unsupported"
+		warning "检测到的环境为 & Detected environment was :-"
+		warning "系统名称 & os_name:'$os_name'"
+		warning "系统类型 & os_mode:'$os_mode'"
+		warning "CPU & cpu_name:'$cpu_name'"
+		warning "CPU结构 & cpu_architecture:'$cpu_architecture'"
 		exit 3
 	fi
 else
-	error "You are using an unsupported architecture '$cpu_architecture'"
-	warning "Detected environment was :-"
-	warning "os_name:'$os_name'"
-	warning "os_mode:'$os_mode'"
-	warning "cpu_name:'$cpu_name'"
+	error "您使用的是不支持的系统结构 & You are using an unsupported architecture '$cpu_architecture'"
+	warning "检测到的环境为 &  Detected environment was :-"
+	warning "系统名称 & os_name:'$os_name'"
+	warning "系统类型 &  os_mode:'$os_mode'"
+	warning "CPU & cpu_name:'$cpu_name'"
 	exit 3
 fi
